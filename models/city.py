ef71a982919a49c4f7b13d237a9093b6bb9895fa
ramsteven <ramsteven@gmail.com>
2021-03-31 00:10:55 -0500

    adding city class

--- models/city.py
+++ models/city.py
@@ -13,7 +13,6 @@ class City(BaseModel, Base):
     if os.getenv('HBNB_TYPE_STORAGE') == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        state = relationship('State')
         places = relationship('Place', cascade="delete")
     else:
         state_id = ""
